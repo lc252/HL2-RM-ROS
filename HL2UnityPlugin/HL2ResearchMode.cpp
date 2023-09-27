734602e654f0a25204d2ff2e27e381567a41d5ba
Lachlan <lc252@uowmail.edu.au>
2023-09-27 17:16:11 +1000

    frame fixed

--- HL2UnityPlugin/HL2ResearchMode.cpp
+++ HL2UnityPlugin/HL2ResearchMode.cpp
@@ -340,9 +340,10 @@ namespace winrt::HL2UnityPlugin::implementation
                                 if (!pHL2ResearchMode->m_useRoiFilter ||
                                     (pHL2ResearchMode->m_useRoiFilter && XMVector3InBounds(tempPoint - roiCenter, roiBound)))
                                 {
+                                    // in the ros frame, (-x, z, -y)
+                                    pointCloud.push_back(-XMVectorGetZ(pointInWorld));
                                     pointCloud.push_back(-XMVectorGetX(pointInWorld));
                                     pointCloud.push_back(XMVectorGetY(pointInWorld));
-                                    pointCloud.push_back(-XMVectorGetZ(pointInWorld));
                                 }
                             }
                             // else if (i > pHL2ResearchMode->depthCamRoi.kRowLower * resolution.Height && i < pHL2ResearchMode->depthCamRoi.kRowUpper * resolution.Height &&
